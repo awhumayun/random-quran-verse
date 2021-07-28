233bb7f7df63bbfb2d966e554a2b159cc02ee8f0
ahumayun <abd.humayun@gmail.com>
2021-07-28 19:12:14 -0400

    Add the translation classes

---
+++ src/app/classes/translation-data.ts
@@ -0,0 +1,48 @@
+import { Translation } from "./translation";
+
+export const TRANSLATIONS: Translation[] = [
+    {
+        id: "en.ahmedali",
+        name: "Ahmed Ali"
+    },
+    {
+        id: "en.ahmedraza",
+        name: "Ahmed Raza Khan"
+    },
+    {
+        id: "en.arberry",
+        name: "Arberry"
+    },
+    {
+        id: "en.asad",
+        name: "Asad"
+    },
+    {
+        id: "en.daryabadi",
+        name: "Daryabadi"
+    },
+    {
+        id: "en.hilali",
+        name: "Hilali & Khan"
+    },
+    {
+        id: "en.pickthall",
+        name: "Pickthall"
+    },
+    {
+        id: "en.qaribullah",
+        name: "Qaribullah & Darwish"
+    },
+    {
+        id: "en.sahih",
+        name: "Saheeh International"
+    },
+    {
+        id: "en.sarwar",
+        name: "Sarwar"
+    },
+    {
+        id: "en.yusufali",
+        name: "Yusuf Ali"
+    }
+];
