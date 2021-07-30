b3b0ae3080ae509099e6ffd85caa5d559c4638cc
ahumayun <abd.humayun@gmail.com>
2021-07-29 23:16:56 -0400

    Change css files to scss files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { VerseService } from './services/verse.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'randomquranverse';
