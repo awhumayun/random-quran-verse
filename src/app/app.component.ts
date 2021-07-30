e4fc8a9139c94b8f98a50ad31412b6b01f260a88
ahumayun <abd.humayun@gmail.com>
2021-07-29 20:04:49 -0400

    Get a random verse on init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Verse } from './interfaces/verse';
+import { TRANSLATIONS } from '../app/classes/translation-data';
 import { VerseService } from './services/verse.service';
 
 @Component({
@@ -10,7 +11,22 @@ import { VerseService } from './services/verse.service';
 export class AppComponent {
   title = 'randomquranverse';
 
+  numOfVerses = 6236;
+  
+  verse!: Verse;
+  verseTranslation!: Verse;
+  translation = TRANSLATIONS[6];
+
   constructor(private verseService: VerseService) { }
 
-  ngOnInit() { }
+  async ngOnInit() { 
+    await this.getRandomVerse();
+  }
+
+  async getRandomVerse(): Promise<void> {
+    const random = Math.floor(Math.random() * this.numOfVerses) + 1;
+
+    this.verse = await this.verseService.getVerse(random);
+    this.verseTranslation = await this.verseService.getVerseTranslation(random, this.translation);
+  }
 }
