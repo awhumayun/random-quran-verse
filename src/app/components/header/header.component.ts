9ed874ec0cae47d691cb30771bb4ad1b28f50fb8
ahumayun <abd.humayun@gmail.com>
2021-08-03 16:57:02 -0400

    Make the site mobile-friendly

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  logo = {
+    bold: 'Random',
+    text: ' Quran Verse'
+  };
+
+  isOnMobile: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.setMobile();
   }
 
+  setMobile(): void {
+    if(screen.width <= 600) {
+      this.isOnMobile = true;
+      
+      this.logo.bold = 'R';
+      this.logo.text = 'QV'
+    }
+  }
 }
