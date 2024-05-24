10cfadc8259096d83ad08c84e928b5ee93a47cfc
awhumayun <abd.humayun@gmail.com>
2024-05-24 16:13:47 -0400

    Add search engine optimization

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,25 +6,7 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./header.component.scss"],
 })
 export class HeaderComponent implements OnInit {
-  logo = {
-    bold: "Random",
-    text: " Quran Verse",
-  };
-
-  isOnMobile: boolean = false;
-
   constructor() {}
 
-  ngOnInit(): void {
-    this.setMobile();
-  }
-
-  setMobile(): void {
-    if (screen.width <= 600) {
-      this.isOnMobile = true;
-
-      this.logo.bold = "R";
-      this.logo.text = "QV";
-    }
-  }
+  ngOnInit(): void {}
 }
